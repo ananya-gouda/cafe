8a1eb149cfaefd74ef255b1a5c8ead057d7d2af4
ananya-gouda <ananyaggouda01@gmail.com>
2023-11-16 19:08:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Menu from "./Components/Menu";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
+import {Routes, Route} from 'react-router-dom';
+import Footer from "./Components/Footer";
+
+  
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Routes>
+         <Route exact path="/landing-page" element={<Home />} />
+         <Route exact path="/home" element={<Home />} />
+         <Route exact path="/about" element={<About />} />
+         <Route exact path="/menu" element={<Menu />} />
+         <Route exact path="/contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
